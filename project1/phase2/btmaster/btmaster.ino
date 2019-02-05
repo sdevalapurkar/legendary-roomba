13eb8090ffeb75f701dc0be88fc35a83a67b680b
Rickus <rickussenekal@gmail.com>
2019-02-05 12:46:03 -0800

    add TTA scheduling into btmaster

--- project1/phase2/btmaster/btmaster.ino
+++ project1/phase2/btmaster/btmaster.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include "scheduler.h"
 
 int joyRX = A1;
 int joyRY = A0;
@@ -13,9 +14,23 @@ int joyRbuttonState = 0;
 int laserPin = 2;
 boolean toggle = true;
 int sensorValue[3];
+uint8_t idle_pin = 7;
 
 const int rs = 44, en = 45, d4 = 40, d5 = 41, d6 = 38, d7 = 39;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+void idle(uint32_t idle_period)
+{
+  // this function can perform some low-priority task while the scheduler has nothing to run.
+  // It should return before the idle period (measured in ms) has expired.  For example, it
+  // could sleep or respond to I/O.
+ 
+  // example idle function that just pulses a pin.
+  digitalWrite(idle_pin, HIGH);
+  delay(idle_period);
+  digitalWrite(idle_pin, LOW);
+}
 
 
 void setup () {
@@ -25,12 +40,26 @@ void setup () {
   pinMode (joyRY, INPUT); 
   pinMode (joyRbutton, INPUT_PULLUP); 
   pinMode (lightPin, INPUT);
+  pinMode (7, OUTPUT);
   lcd.begin(16, 2);
-  lcd.clear();                 
+  lcd.clear();
+
+  Scheduler_Init();
+
+  Scheduler_StartTask(0, 50, readSensors);
+  Scheduler_StartTask(0, 200, sendValues);
+  Scheduler_StartTask(0, 50, setCursorZero);
+  Scheduler_StartTask(0, 200, checkLightSensor);
+  Scheduler_StartTask(0, 50, setCursorOne);
+  Scheduler_StartTask(0, 50, checkJoyRAxis);
 }
 
 
 void readSensors(){
+  lightSensorValue = analogRead(lightPin);
+  x_pos = analogRead(joyRX);  
+  y_pos = analogRead(joyRY);
+  
   sensorValue[0] = digitalRead(joyRbutton);
   sensorValue[1] = analogRead(joyRX);  
   sensorValue[2] = analogRead(joyRY);
@@ -46,54 +75,94 @@ void sendValues(){
   }
   Serial1.print('*');
   Serial1.println();
-  delay(10);
 }
 
-void loop ( ) {
-  readSensors();
-  sendValues();
-  
-  lightSensorValue = analogRead(lightPin);
-  x_pos = analogRead(joyRX);  
-  y_pos = analogRead(joyRY);
-  
-  lcd.setCursor(0, 0);
-  if (lightSensorValue > 900){
-    lcd.print("HIT             ");
-  } else{
-    lcd.print("ALIVE            ");
-  }
 
-  lcd.setCursor(0,1);
+void checkLightSensor(){
+  if (lightSensorValue > 900){
+    lcdPrint("HIT             ");
+  } else{
+    lcdPrint("ALIVE            ");
+  }
+}
+
+
+void lcdPrint(String text){
+  lcd.print(text);
+}
+
+
+void checkJoyRAxis(){
   if (x_pos < 300){
     if (initial_positionX < 10) { 
     } else { 
-      lcd.print("RIGHT           "); 
-      delay(50); 
+      lcdPrint("RIGHT           "); 
+//      delay(50); 
     } 
   } 
   
   if (x_pos > 700){
     if (initial_positionX > 180){  
     } else {
-      lcd.print("LEFT             ");
-      delay(50);
+      lcdPrint("LEFT             ");
+//      delay(50);
     }
   }
-  
+
   if (y_pos < 300){
     if (initial_positionY < 10) { 
     } else { 
-      lcd.print("UP               ");
-      delay(50); 
+      lcdPrint("UP               ");
+//      delay(50); 
       } 
    } 
   
   if (y_pos > 700){
     if (initial_positionY > 180){  
     } else{
-      lcd.print("DOWN             ");
-      delay(50);
+      lcdPrint("DOWN             ");
+//      delay(50);
     }
   }
+}
+
+
+void setCursorZero(){
+  lcd.setCursor(0, 0);
+}
+
+
+void setCursorOne(){
+  lcd.setCursor(0, 1);
+}
+
+
+void loop() {
+
+  uint32_t idle_period = Scheduler_Dispatch();
+  if (idle_period)
+  {
+    idle(idle_period);
+  }
+  
+//  readSensors();
+//  sendValues();
+//  
+//  setCursors(0, 0);
+//  checkLightSensor();
+//
+//  setCursors(0,1);
+//  checkJoyRAxis();
+}
+
+int main() {
+  init();
+  setup();
+ 
+  for (;;)
+  {
+    loop();
+  }
+  for (;;);
+  return 0;
 }
