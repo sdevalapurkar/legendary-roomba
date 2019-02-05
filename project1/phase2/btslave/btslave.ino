3800fe778c5c72f8df9af7861a2a7e0e539ce1af
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-02-05 12:40:31 -0800

    add completed btslave code

--- project1/phase2/btslave/btslave.ino
+++ project1/phase2/btslave/btslave.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include "scheduler.h"
 
 String readStringg; //main captured String 
 String joyRButtonState; //data String
@@ -15,6 +16,7 @@ Servo servoX;
 Servo servoY;
 int servoY_pin = 8;
 int servoX_pin = 9;
+uint8_t idle_pin = 7;
  
 void setup() {
   pinMode(laserPin, OUTPUT);
@@ -24,9 +26,74 @@ void setup() {
   servoX.attach(servoX_pin);
   servoY.write(initial_positionY);
   servoX.write(initial_positionX);
+
+  Scheduler_Init();
+ 
+  Scheduler_StartTask(0, 10, readValues);
+  Scheduler_StartTask(1, 5, sendLaser);
+  Scheduler_StartTask(0, 50, moveServo);
 }
 
-void loop() {
+// idle task
+void idle(uint32_t idle_period)
+{
+  // this function can perform some low-priority task while the scheduler has nothing to run.
+  // It should return before the idle period (measured in ms) has expired.  For example, it
+  // could sleep or respond to I/O.
+ 
+  // example idle function that just pulses a pin.
+  digitalWrite(idle_pin, HIGH);
+  delay(idle_period);
+  digitalWrite(idle_pin, LOW);
+}
+
+void sendLaser() {
+  if (joyRButtonState.toInt() == 0) {
+    if (toggle) {
+      digitalWrite(laserPin, HIGH);   // set the LED on
+      toggle = !toggle;
+    } else {
+      digitalWrite(laserPin, LOW);    // set the LED off
+      toggle = !toggle;
+    }
+  }
+}
+
+void moveServo() {
+  if (joyRX.toInt() < 300) {
+    if (initial_positionX < 10) { 
+    } else { 
+      initial_positionX = initial_positionX - 1; 
+      servoX.write (initial_positionX); 
+    } 
+  } 
+
+  if (joyRX.toInt() > 700) {
+    if (initial_positionX > 180) {  
+    } else {
+      initial_positionX = initial_positionX + 1;
+      servoX.write (initial_positionX);
+    }
+  }
+
+  if (joyRY.toInt() < 300){
+    if (initial_positionY < 10) { 
+    } else { 
+      initial_positionY = initial_positionY - 1; 
+      servoY.write (initial_positionY);
+    } 
+  } 
+
+  if (joyRY.toInt() > 700){
+    if (initial_positionY > 180){  
+    } else{
+      initial_positionY = initial_positionY + 1;
+      servoY.write (initial_positionY);
+    }
+  }
+}
+
+void readValues() {
   if (Serial1.available())  {
     char c = Serial1.read();  //gets one byte from serial buffer
     
@@ -37,61 +104,31 @@ void loop() {
       joyRX = readStringg.substring(ind1+1, ind2);   //captures second data String
       ind3 = readStringg.indexOf(',', ind2+1 );
       joyRY = readStringg.substring(ind2+1, ind3);
-
-      if (joyRButtonState.toInt() == 0) {
-        delay(50);  //delay for debounce
-    
-        if (toggle) {
-          digitalWrite(laserPin, HIGH);   // set the LED on
-          toggle = !toggle;
-        } else {
-          digitalWrite(laserPin, LOW);    // set the LED off
-          toggle = !toggle;
-        }
-      }
-
-      if (joyRX.toInt() < 300) {
-        if (initial_positionX < 10) { 
-        } else { 
-          initial_positionX = initial_positionX - 1; 
-          servoX.write (initial_positionX);
-          delay(50); 
-        } 
-      } 
-  
-      if (joyRX.toInt() > 700) {
-        if (initial_positionX > 180) {  
-        } else {
-          initial_positionX = initial_positionX + 1;
-          servoX.write (initial_positionX);
-          delay(50);
-        }
-      }
-  
-      if (joyRY.toInt() < 300){
-        if (initial_positionY < 10) { 
-        } else { 
-          initial_positionY = initial_positionY - 1; 
-          servoY.write (initial_positionY);
-          delay(50); 
-        } 
-      } 
-  
-      if (joyRY.toInt() > 700){
-        if (initial_positionY > 180){  
-        } else{
-          initial_positionY = initial_positionY + 1;
-          servoY.write (initial_positionY);
-          delay(50);
-        }
-      }
       
       readStringg=""; //clears variable for new input
-      joyRButtonState="";
-      joyRX="";
-      joyRY="";
-    } else {     
+    } else {
       readStringg += c; //makes the string readString
     }
   }
+}
+
+void loop() {
+  uint32_t idle_period = Scheduler_Dispatch();
+  if (idle_period)
+  {
+    idle(idle_period);
+  }
+}
+
+int main()
+{
+  init();
+  setup();
+ 
+  for (;;)
+  {
+    loop();
+  }
+  for (;;);
+  return 0;
 }
